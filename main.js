63bfdcc49e4e6797462edc3e600c47fd87eaa0e7
Wiliam Muller <174868022+SillyWili@users.noreply.github.com>
2025-08-20 06:29:09 +0200

    all methods work has intended

--- main.js
+++ main.js
@@ -14,5 +14,39 @@ test.set("ice cream", "white");
 test.set("jacket", "blue");
 test.set("kite", "pink");
 test.set("lion", "golden");
+console.log(test.length()); // 12
+console.log(test.entries());
 
+console.log("overwrite: ");
+
+test.set("apple", "yellow");
+test.set("dog", "white");
+
+console.log(test.length()); // same number: 12
+console.log(test.entries());
+
+console.log("Grow functionality test:");
+console.log(test.growFactor());
+
+test.set("moon", "silver");
+console.log(test.visualizeBucketsStatus());
+console.log(test.length()); // 13
+console.log(test.entries());
+console.log(test.capacity);
+
+test.set("moon", "white");
+test.set("dog", "red");
+console.log(test.visualizeBucketsStatus());
+console.log(test.length()); // 13
+console.log(test.entries());
+console.log(test.capacity);
+
+console.log("Other methods test");
+console.log(test.get("dog"));
+console.log(test.has("moon"));
+console.log(test.remove("lion"));
+console.log(test.entries());
+console.log(test.values());
+console.log(test.keys());
+test.clear();
 console.log(test.entries());
